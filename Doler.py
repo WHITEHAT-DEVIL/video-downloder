9b71a4c542daeb2d34d035a758891c752cebe44a
DEVIL <77931214+WHITEHAT-DEVIL@users.noreply.github.com>
2021-02-25 13:54:22 +0530

    Update

--- Doler.py
+++ Doler.py
@@ -1,1 +1,14 @@
-
+# video-downloder
+from pytube import YouTube 
+>> printf("     #########   ##          ##  ##########   ##       "\n)
+   printf("     ##      ##   ##        ##       ##       ##       "\n)
+   printf("     ##      ##    ##      ##        ##       ##       "\n)
+   printf("     ##      ##     ##    ##         ##       ##        "\n)
+   printf("     ##      ##      ##  ##          ##       ##        "\n)
+   printf("     #########         ##        ##########   ######### "\n)
+  
+  printf(" This code is create by:--WHITEHAT-DEVIL/video-downloder/github.com ")
+link = input("Enter the link:")
+video = YouTube(link)
+stream = video.streams.get_highest_resolution()
+stream.download()
